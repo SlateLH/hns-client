76458d705918e46f5a031b58c4e9c100b9c7f97c
Slate Hayes <slatelhayes@gmail.com>
2022-02-06 22:18:24 -0600

    Enable tailwind jit and change chat sender colors

--- src/components/Game/GameLobby/GameLobbyChat/GameLobbyChatListItem.tsx
+++ src/components/Game/GameLobby/GameLobbyChat/GameLobbyChatListItem.tsx
@@ -8,9 +8,17 @@ interface Props {
 }
 
 function GameLobbyChatListItem({ index, chatMessage }: Props) {
+  const senderColor = chatMessage.sender.split(' ')[0].toLowerCase();
+
   return (
     <li className="p-2 mb-1 border rounded-lg bg-gray-100">
-      <h5 className="font-semibold">{chatMessage.sender}</h5>
+      <h5
+        className={`font-semibold text-${
+          senderColor === 'white' ? 'black' : senderColor
+        }-500`}
+      >
+        {chatMessage.sender}
+      </h5>
       <p>{chatMessage.message}</p>
       <h6 className="font-light text-xs text-gray-400 text-right">
         {chatMessage.time}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'jit',
   content: [path.resolve(__dirname, 'src', '**', '*')],
+  purge: {
+    options: {
+      safelist: ['text-lime-500'],
+    },
+  },
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'aqua-500': '#00ffff',
+        'olive-500': '#808000',
+      },
+    },
   },
   plugins: [],
 };
